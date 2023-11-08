e2900ebcbbe58e6fc997844d5df23764889b0319
divy-arun-mav <113162746+divy-arun-mav@users.noreply.github.com>
2023-11-08 11:08:03 +0530

    Update Payment.js

--- Frontend/src/Components/Payment.js
+++ Frontend/src/Components/Payment.js
@@ -1,14 +1,39 @@
 import React, { useEffect, useState } from 'react'
 
 function Payment() {
+
+    useEffect(() => {
+        window.alert("Logged In To The Wallet before Doing Any Transaction");
+    }, [])
+
+    const [iAmt, setAmt] = useState("");
+    const [iAdd, setAdd] = useState("");
+
+    const openTronLinkWallet = async () => {
+        if (window.tronWeb && window.tronWeb.defaultAddress.base58) {
+            const toAddress = iAdd;
+            const amount = iAmt * 1e6;
+
+            try {
+                await window.tronWeb.trx.sendTransaction(toAddress, amount);
+                window.alert("Funds Added Successfully!!!")
+            } catch (error) {
+                console.error('Error sending transaction:', error);
+            }
+        } else {
+            alert('Please install and log in to TronLink wallet to initiate the transaction.');
+        }
+    };
+
     return (
         <>
             <div className='box'>
                 <div className='head-1'>
                     <h2>Crypto Payments</h2>
                 </div>
-                <div className='form'>
+                <div className='htmlForm'>
                     <h3>Merchant Dashboard</h3>
+
                     <div className="chain">
                         <label for="chain">Choose a Chain:</label>
                         <select id="chain" name="chain">
@@ -37,11 +62,11 @@ function Payment() {
                         <input id="address" placeholder="wallet address" />
                     </div>
 
-                    <button>Generate</button>
+                    <button onClick={openTronLinkWallet}>Generate</button>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default Payment
