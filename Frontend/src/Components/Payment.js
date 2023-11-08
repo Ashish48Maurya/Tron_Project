6257c94236335290b2b5edcd14c25c30bc9c4c94
divy-arun-mav <113162746+divy-arun-mav@users.noreply.github.com>
2023-11-08 10:33:41 +0530

    Changes in payment.js

--- Frontend/src/Components/Payment.js
+++ Frontend/src/Components/Payment.js
@@ -1,55 +1,47 @@
-import React, { useEffect, useState } from 'react';
-
+import React from 'react'
 
 function Payment() {
+    return (
+        <>
+            <div className='box'>
+                <div className='head-1'>
+                    <h2>Crypto Payments</h2>
+                </div>
+                <div className='form'>
+                    <h3>Merchant Dashboard</h3>
+                    <div className="chain">
+                        <label for="chain">Choose a Chain:</label>
+                        <select id="chain" name="chain">
+                            <option value="eth">ETH</option>
+                        </select>
+                    </div>
 
-  useEffect(()=>{
-    window.alert("Logged In To The Wallet before Doing Any Transaction");
-  },[])
+                    <div className="asset">
+                        <label for="asset">Payment Asset Type:</label>
+                        <select id="asset" name="asset">
+                            <option value="eth">TRX</option>
+                            <option value="usdc">USDC</option>
+                            <option value="usdt">USDT</option>
+                            <option value="eth">ETH</option>
+                            <option value="btc">BTC</option>
+                        </select>
+                    </div>
 
-  const [iAmt, setAmt] = useState("");
-  const [iAdd, setAdd] = useState("");
+                    <div className="amount">
+                        <label for="amount">Enter Amount:</label>
+                        <input id="amount" placeholder="amount" />
+                    </div>
 
-  const openTronLinkWallet = async () => {
-    if (window.tronWeb && window.tronWeb.defaultAddress.base58) {
-      const toAddress = iAdd; 
-      const amount = iAmt*1e6;
+                    <div className="address">
+                        <label for="address">Enter Wallet Address:</label>
+                        <input id="address" placeholder="wallet address" />
+                    </div>
 
-      try {
-        await window.tronWeb.trx.sendTransaction(toAddress, amount);
-        window.alert("Funds Added Successfully!!!")
-      } catch (error) {
-        console.error('Error sending transaction:', error);
-      }
-    } else {
-      alert('Please install and log in to TronLink wallet to initiate the transaction.');
-    }
-  };
-
-  return (
-    <>
-      <div className='box'>
-        <div className='head-1'>
-          <h2>Crypto Payments</h2>
-        </div>
-        <div className='htmlForm'>
-          <h3>Merchant Dashboard</h3>
-
-          <div className="amount">
-            <label htmlFor="amount">Enter Amount:</label>
-            <input id="amount" onChange={(e) => { setAmt(e.target.value) }} value={iAmt} placeholder="amount" />
-          </div>
-
-          <div className="address">
-            <label htmlFor="address">Enter Receiver's Wallet Address:</label>
-            <input id="address" placeholder="wallet address" onChange={(e) => { setAdd(e.target.value) }} value={iAdd} />
-          </div>
-
-          <button onClick={openTronLinkWallet}>Generate</button>
-        </div>
-      </div>
-    </>
-  );
+                    <button>Generate</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Payment;
+export default Payment
