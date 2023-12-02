80f84058b8376d05a03d45219beb146b9d209d57
Divy Mav <divymav5@gmail.com>
2023-12-02 14:13:47 +0530

    new login ad register

--- Frontend/src/Components/Login.js
+++ Frontend/src/Components/Login.js
@@ -1,18 +1,57 @@
 import React, { useState } from 'react';
-import { toast } from 'react-toastify';
-import { useNavigate, Link } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
 
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
-const Login = () => {
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function Login() {
     const navigate = useNavigate();
-    const [username, setUserName] = useState("");
-    const [password, setPassword] = useState("");
 
     const notifyA = (msg) => toast.error(msg);
     const notifyB = (msg) => toast.success(msg);
 
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const passRege = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+
     const postData = async () => {
+
+        if (!emailRegex.test(email)) {
+            notifyA("Invalid Email");
+            return;
+        }
+        else if (!passRege.test(password)) {
+            notifyA("Password must contain atleast 8 characters, including atleast 1 number and 1 includes both lower and uppercase letters and special characters for example #,?!");
+            return;
+        }
+
         try {
             fetch("http://localhost:8000/signin", {
                 method: "post",
@@ -20,6 +59,8 @@ const Login = () => {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
+                    name: name,
+                    email: email,
                     username: username,
                     password: password
                 })
@@ -29,7 +70,7 @@ const Login = () => {
                         notifyA(data.error);
                     } else {
                         notifyB(data.message);
-                        navigate('/admin')
+                        navigate('/')
                     }
                     console.log(data)
                 })
@@ -38,55 +79,78 @@ const Login = () => {
         }
     }
 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-
-        <div className="container">
-            <div className="row d-flex">
-                <div className="col-lg-6 col-md-8 col-sm-12">
-                    <div className="left">
-                        <img
-                            src="images/login.png"
-                            alt="login"
-                            className="img-fluid"
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={postData} sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
                         />
-                    </div>
-                </div>
-                <div className="col-lg-6 col-md-8 col-sm-12">
-                    <div className="right" >
-                        <div className="login-form">
-                            <h3 className="header">LOGIN</h3>
-                            <input
-                                type="text"
-                                placeholder="Enter Username"
-                                name="username"
-                                required=""
-                                onChange={(e) => { setUserName(e.target.value) }}
-                                value={username}
-                            />
-                            <input
-                                type="password"
-                                placeholder="Enter Password"
-                                name="password"
-                                required=""
-                                onChange={(e) => { setPassword(e.target.value) }}
-                                value={password}
-                            />
-                            <div className='btn-login-grp'>
-                            <Button variant="contained" color="primary" onClick={postData}>
-                                Log In
-                                </Button>
-
-                            <Link to="/register" className="reglink">
-                                Sign Up
-                            </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    )
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="/register" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
+    );
 }
-
-export default Login;
--- Frontend/src/Components/Register.js
+++ Frontend/src/Components/Register.js
@@ -1,153 +1,206 @@
 import React, { useState } from 'react';
-import { toast } from 'react-toastify';
-import { useNavigate } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import { generate, count } from "random-words"
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import { generate, count } from "random-words";
 
-const Register = () => {
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
+// TODO remove, this demo shouldn't need to reset the theme.
 
+const defaultTheme = createTheme();
+
+export default function Register() {
     const navigate = useNavigate();
 
+    //Toast functions
     const notifyA = (msg) => toast.error(msg);
     const notifyB = (msg) => toast.success(msg);
-
     const passRege = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
 
-  const postData = async () => {
-    if (!username || !password || !phrase) {
-      return notifyA("All Fields Are Required!!!");
-    }
-    else if (!passRege.test(password)) {
-      notifyA("Password must contain atleast 8 characters, including atleast 1 number and 1 includes both lower and uppercase letters and special characters for example #,?!");
-      return;
+    const postData = async () => {
+        if (!username || !password || !phrase) {
+            return notifyA("All Fields Are Required!!!");
+        }
+        else if (!passRege.test(password)) {
+            notifyA("Password must contain atleast 8 characters, including atleast 1 number and 1 includes both lower and uppercase letters and special characters for example #,?!");
+            return;
+        }
+
+        try {
+            fetch("http://localhost:8000/register", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                    phrase: phrase
+                })
+            }).then(res => res.json())
+                .then(data => {
+                    if (data.error) {
+                        notifyA(data.error);
+                    } else {
+                        notifyB(data.message);
+                        navigate('/')
+                    }
+                    console.log(data)
+                })
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    try {
-        fetch("http://localhost:8000/register", {
-            method: "post",
+    const setUsername = async () => {
+        const response = await fetch("http://localhost:8000/username", {
+            method: "GET",
             headers: {
-                "Content-Type": "application/json",
+                Accept: "application/json",
             },
-            body: JSON.stringify({
-                username: username,
-                password: password,
-                phrase: phrase
-            })
-        }).then(res => res.json())
-            .then(data => {
-                if (data.error) {
-                    notifyA(data.error);
-                } else {
-                    notifyB(data.message);
-                    navigate('/')
-                }
-                console.log(data)
-            })
-    } catch (error) {
-        console.log(error);
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            setUserName(data.username);
+        } else {
+            window.alert("Server Busy, Try Again Later");
+        }
     }
-}
 
-const setUsername = async () => {
-    const response = await fetch("http://localhost:8000/username", {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-        },
-    });
-
-    if (response.status === 200) {
-        const data = await response.json();
-        setUserName(data.username);
-    } else {
-        window.alert("Server Busy, Try Again Later");
+    const setPrase = async () => {
+        const phrase = generate({ exactly: 8, join: " ", maxLength: 6 });
+        setPhrase(phrase);
     }
-}
 
-const setPrase = async () => {
-    const phrase = generate({ exactly: 8, join: " ", maxLength: 6 });
-    setPhrase(phrase);
-}
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [phrase, setPhrase] = useState("");
 
-const [username, setUserName] = useState("");
-const [password, setPassword] = useState("");
-const [phrase, setPhrase] = useState("");
-
-return (
-
-
-    <div className="container">
-        <div className="row d-flex">
-            <div className="col-lg-6 col-md-8 col-sm-12">
-                <div className="left">
-                    <img
-                        src="images/signup.png"
-                        alt="signup"
-                        className="img-fluid"
-                    />
-                </div>
-            </div>
-            <div className="col-lg-6 col-md-8 col-sm-12">
-                <div className="right w-60">
-
-                    <h1>Sign Up</h1>
-                    <p>Please fill in this form to create an account.</p>
-                    <hr />
-                    <label htmlFor="name">
-                        <b>Username</b>
-                    </label>
-                    <div><input
-                        type="text"
-                        placeholder="✍️ username ✍️"
-                        name="name"
-                        required=""
-                        onChange={(e) => { setUserName(e.target.value) }}
-                        value={username}
-                    />
-                        <Button variant="contained" color="primary" onClick={setUsername}>
-                            Generate Username
-                        </Button></div>
-                    <label htmlFor="password">
-                        <b>Password</b>
-                    </label>
-                    <div>
-                        <input
-                            className='w-90'
-                            type="password"
-                            placeholder="✍️ password ✍️"
-                            name="password"
-                            required=""
-                            onChange={(e) => { setPassword(e.target.value) }}
-                            value={password}
-                        />
-
-                    </div>
-                    <label htmlFor="name">
-                        <b>Secret Phrase</b>
-                    </label>
-                    <div>
-                        <input
-                            type="text"
-                            contentEditable="false"
-                            name="phrase"
-                            required=""
-                            value={phrase}
-                        />
-                        <Button variant="contained" color="primary" onClick={setPrase}>
-                            Generate Phrase
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={postData} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="Name"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="Username"
+                                    autoFocus
+                                    onChange={(e) => { setUserName(e.target.value) }}
+                                    value={username}
+                                />
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    onClick={setUsername}
+                                >
+                                    Generate Username
+                                </Button>
+                            </Grid> 
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Phrase"
+                                    name="email"
+                                    type='text'
+                                    contentEditable="false"
+                                    value={phrase}
+                                />
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    onClick={setPrase}
+                                >
+                                    Generate Phrase
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    onChange={(e) => { setPassword(e.target.value) }}
+                                    value={password}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControlLabel
+                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                    label="I want to receive inspiration, marketing promotions and updates via email."
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
                         </Button>
-                    </div>
-                    <Button variant="contained" color="success" style={{ "paddingInline": "20px" }} onClick={postData}>
-                        Success
-                    </Button>
-
-
-                </div>
-            </div>
-        </div>
-    </div>
-)
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider>
+    );
 }
-
-export default Register;
