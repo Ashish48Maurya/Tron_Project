4ad9f28c1787ab6bd6248dc83d45567bed20eb1d
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2023-12-21 08:28:40 +0530

    About us page added

--- Frontend/src/Components/About.js
+++ Frontend/src/Components/About.js
@@ -3,9 +3,51 @@ import Navbar from './Navbar'
 
 const About = () => {
   return (
+    <>
       <div>
           <Navbar/>
+          <section class="hero">
+            <div class="heading">
+              <h1>About Us</h1>
+            </div>
+            <div class="container">
+              <div class="hero content"></div>
+              <h2>Welcome to Payonweb</h2>
+              <p>Experience seamless and rapid digital exchanges through Payonweb, ensuring swift and efficient transactions that enhance your online interactions. Payonweb empowers you to navigate the digital realm effortlessly, providing a frictionless pathway to your desired transactions with ease and speed.</p>
+              <button class="cta-button">Learn More</button>
+            </div>
+            <div class="hero-image">
+              <img src="Frontend/public/images/20547381_6280661.jpg"/>
+            </div>
+          </section>
     </div>
+    <style>
+        {`
+        *{
+          margin:0px;
+          padding:0px;
+          box-sizing: border-box;
+          font-family:"Arial",san-serif; 
+        }
+        .hero{
+          background-color:f8f8f8;
+          overflow: hidden;
+        }
+        .heading h1{
+          color:#ff6347;
+          font-size:55px;
+          text-align:center;
+          margin-top:35px;
+        }
+        .container{
+         display: flex;
+         justify-content: center;
+         align-items:center; 
+        }
+          
+        `}
+      </style>
+    </>
   )
 }
 
