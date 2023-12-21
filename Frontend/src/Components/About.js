e464676d3f828577ea96a64bebaff951a207c3d8
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2023-12-21 09:54:38 +0530

    about us updated

--- Frontend/src/Components/About.js
+++ Frontend/src/Components/About.js
@@ -44,7 +44,75 @@ const About = () => {
          justify-content: center;
          align-items:center; 
         }
+         .hero-content{
+          flex:1;
+          width:600px;
+          margin: 0px 25px
+         }
+         .hero-content h2{
+            font-size:38px;
+            margin-bottom: 20px;
+            color:#333;
+         } 
+         .hero-content p{
+            font-size: 24px;
+            line-height: 1.5;
+            margin-bottom: 40px;
+            color:#666;
           
+         }
+         .hero-content button{
+          display: inline-block;
+          background-colour:#ff4500;
+          color:#fff;
+          padding: 12px 24px;
+          border-radius: 5px;
+          font-size:20px;
+          border: none;
+          cursor: pointer;
+          transition: 0.3s ease;
+         }
+         .hero-content button:hover{
+            background-color:#ff6347;
+            transform: scale(1.1);
+         }
+         .hero-image{
+           flex:1;
+           width:600px;
+           margin: auto;
+
+         }
+         img{
+          width:100%;
+          height:auto;
+          border-radius: 10px;
+         }
+         @media screen and (max-width: 768px){
+          .heading h1{
+            font-size: 45px;
+            margin-top: 30px;
+          }
+          .hero{
+            margin: 0px;
+          }
+          .container{
+            width:100%;
+            flex-direction: column;
+            margin:0px;
+            padding:0px 40px;
+          }
+          .hero-content{
+            width:100%;
+            margin:35px 0px;
+          }
+          .hero-content h2{
+            font-size:30px;         
+          }
+          .hero-content p{
+            font-size:18px;
+            margin-bottom: 20px;
+          }
+         }
         `}
       </style>
     </>
